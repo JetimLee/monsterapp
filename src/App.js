de50878848b3f86e0f1e69c6d7eea0a12b0fdbb0
Gavin Coulson <72872223+JetimLee@users.noreply.github.com>
2021-03-02 15:27:08 +0200

    Update App.js

--- src/App.js
+++ src/App.js
@@ -7,7 +7,8 @@ class App extends Component {
   constructor () {
     super();
     this.state = {
-      monsters: []
+      monsters: [],
+      searchField: '' 
     };
   }
 
@@ -18,9 +19,15 @@ class App extends Component {
   }
 
   render(){
+    const {monsters, searchField } = this.state;
+    const filteredMonsters = monsters.filter(monster => monster.name.toLowerCase().includes(searchField.toLocaleLowerCase())
+    )
     return(
           <div className="App">
-          <CardList monsters={this.state.monsters}/>
+            <input type='search' placeholder='search cats!' onChange={e =>  
+              this.setState({searchField: e.target.value})
+              }></input>
+          <CardList monsters={filteredMonsters} />
           
           </div>
         );
