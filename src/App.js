92a8104532f53869dbde94ada10d207e5ce925ad
Gavin Coulson <72872223+JetimLee@users.noreply.github.com>
2021-03-02 13:36:19 +0200

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,25 +1,31 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+
+import { CardList} from './components/card-list/card-list.component'
 import './App.css';
 
 class App extends Component {
   constructor () {
     super();
     this.state = {
-      string: 'Hello Gavin Coulson'
+      monsters: []
     };
   }
+
+  componentDidMount(){
+    fetch('https://jsonplaceholder.typicode.com/users')
+    .then(response => response.json())
+    .then(users =>this.setState({monsters: users}))
+  }
+
   render(){
     return(
           <div className="App">
-            <header className="App-header">
-              <img src={logo} className="App-logo" alt="logo" />
-              <p>{ this.state.string}</p>
-              <p>{ this.state.string}</p>
-              <p>{ this.state.string}</p>
-
-              <button onClick={() => this.setState({string: 'Hello Sapir'})}>Change text</button>
-            </header>
+          <CardList monsters={this.state.monsters}>
+          {
+            this.state.monsters.map(monster => <h1 key={monster.id}>{ monster.name } </h1>)
+          }
+          </CardList>
+          
           </div>
         );
   }
